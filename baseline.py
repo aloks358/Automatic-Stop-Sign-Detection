72f18e3a253b96a8f4b10cc539fac7014f47bb54
Chaitanya Asawa <casawa@stanford.edu>
2015-10-20 03:08:54 -0700

    Changed baseline_util.py into a baseline script since it is fairly self-contained. More work done on trying to adapt SGD to the case at hand. Still need to do image processing though.

--- baseline.py
+++ baseline.py
@@ -1,11 +1,13 @@
-def getFeatureExtractor():
-    def featureExtractor(image):
-        featureVec = {}
-        for pixel in image:
-            if red:
-                featureVec[pixel] = 1
-        return featureVec
-    return featureExtractor
+DATA_PATH = ""
+NUM_ITERATIONS = 10
+
+def featureExtractor(imagePath):
+    featureVec = {}
+    image =  something imagePath
+    for pixel in image:
+        if red:
+            featureVec[pixel] = 1
+    return featureVec
 
 def dotProduct(v1, v2):
     common_nonzero_indices = [index for index in v1 if index in v2]
@@ -15,22 +17,12 @@ def increment(v1, scale, v2):
     for elem in v2:
         v1[elem] += (scale * v2[elem])
 
-def SGD(trainExamples, testExamples, featureExtractor):
-    '''
-    Given |trainExamples| and |testExamples| (each one is a list of (x,y)
-    pairs), a |featureExtractor| to apply to x, and the number of iterations to
-    train |numIters|, return the weight vector (sparse feature vector) learned.
+def evaluate(examples, classifier):
+    error = 0
 
-    You should implement stochastic gradient descent.
-
-    Note: only use the trainExamples for training!
-    You should call evaluatePredictor() on both trainExamples and testExamples
-    to see how you're doing as you learn after each iteration.
-    numIters refers to a variable you need to declare. It is not passed in.
-    '''
+def SGD(trainExamples, testExamples):
     weights = {}  # feature => weight
-    # BEGIN_YOUR_CODE (around 15 lines of code expected)
-    def grad(weights, trainExample, featureExtractor):
+    def grad(weights, trainExample):
         x = trainExample[0]
         y = trainExample[1]
         features = featureExtractor(x)
@@ -44,15 +36,32 @@ def SGD(trainExamples, testExamples, featureExtractor):
         else:
             return {}
 
-    numIters = 16
+    numIters = NUM_ITERATIONS
     for i in range(numIters):
         step_size = 0.00225
         for trainExample in trainExamples:
-            gradient = grad(weights, trainExample, featureExtractor)
+            gradient = grad(weights, trainExample)
             increment(weights, -step_size, gradient)
 
         # trainError = evaluatePredictor(trainExamples, lambda(x) : (1 if dotProduct(featureExtractor(x), weights) >= 0 else -1))
         # testError = evaluatePredictor(testExamples, lambda(x) : (1 if dotProduct(featureExtractor(x), weights) >= 0 else -1))
         # print trainError, testError
-    # END_YOUR_CODE
     return weights
+
+def get_image_labels():
+	label_tuples = []
+	with open(os.path.join(DATA_PATH, LABEL_FILE), 'r') as label_map_file:
+		for line in label_map_file:
+			split_line = line.split()
+			label_tup = (split_line[0], int(split_line[1]))
+            label_tuples.append(label_tup)
+
+	return label_tuples
+
+def main():
+    trainExamples = get_image_labels()
+    testExamples = []
+    SGD(trainExamples, testExamples) 
+
+if __name__ = "__main__":
+    main()
