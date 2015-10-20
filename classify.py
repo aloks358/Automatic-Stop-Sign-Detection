920727bd7196bf8e5cc5fa92be1474a23b754805
Chaitanya Asawa <casawa@stanford.edu>
2015-10-20 01:49:38 -0700

    Simple testing harness

---
+++ classify.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+DATA_PATH = ""
+
+def classify_image(path):
+	print path
+	return 1
+
+def main():
+	files = [f for f in os.listdir(DATA_PATH) if os.path.isfile(os.path.join(DATA_PATH, f))]
+	for f in files:
+		classifier_label = classify_image(os.path.join(DATA_PATH, f))
+		print "File: ", f, " Classification: ", classifier_label
+
+if __name__ == "__main__":
+	main()
+
