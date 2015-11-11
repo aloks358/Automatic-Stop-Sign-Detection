f2ae2672b79e8d7a86b31a4ad40e01a024342cad
Chaitanya Asawa <casawa@stanford.edu>
2015-11-10 21:10:47 -0800

    Started an image segmenter class

---
+++ image_segmentation.py
@@ -0,0 +1,13 @@
+"""
+This class defines an image segmenter.
+"""
+class ImageSegmenter(object):
+	def __init__(self, numSegments):
+		self.num_segments = numSegments
+
+	"""
+
+	"""
+	def segment():
+		for i in range(self.num_segments):
+			print i
