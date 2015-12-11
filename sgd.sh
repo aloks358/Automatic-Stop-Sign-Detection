d8916009627b92f1a9e4414325dbb76a7037a334
Chaitanya Asawa <casawa@stanford.edu>
2015-12-10 22:49:16 -0800

    Not sure if the previous shell scripts generated unnecessary email traffic for them...

--- sgd.sh
+++ sgd.sh
@@ -8,9 +8,6 @@
 # Tell grid engine to use current directory
 #$ -cwd
 
-# Tell Grid Engine to notify job owner if job 'b'egins, 'e'nds, 's'uspended is 'a'borted, or 'n'o mail
-#$ -m besan
-
 # Tel Grid Engine to join normal output and error output into one file
 #$ -j y
 
